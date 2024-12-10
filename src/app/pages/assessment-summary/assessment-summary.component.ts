3781bd007bf9d8b07b2dceb33efab68161d5fce6
Ignatius A A Saragih <ignatius.a.a.s@gmail.com>
2024-12-10 12:11:52 +0700

    AssSum pt 2

--- src/app/pages/assessment-summary/assessment-summary.component.ts
+++ src/app/pages/assessment-summary/assessment-summary.component.ts
@@ -53,14 +53,10 @@ import { of } from 'rxjs';
   providers: [MessageService],
 })
 export class AssessmentSummaryComponent implements OnInit {
-  loading: boolean = false;
-  isProcessing: boolean = false;
   maxDate: Date = new Date();
   employees: any[] = [];
   currentUserId: string = this.extractCurrentUserId() || '';
-  editForm!: FormGroup;
   currentRoles: any[] = this.extractCurrentRoles() || [];
-  devPlans: any[] = [];
   selectedUserId: string = '';
   selectedName: string = '';
   selectedAssessmentYear: Date = new Date();
@@ -71,19 +67,19 @@ export class AssessmentSummaryComponent implements OnInit {
   suggestion: string = '';
   assessmentYear: Date | null = null;
   totalScore: number = 0;
+  selectedDivision: string = '';
+  selectedPosition: string = '';
+  isLoading: boolean = true;
 
   constructor(
     private http: HttpClient,
     private messageService: MessageService,
-    private fb: FormBuilder,
     private primengConfig: PrimeNGConfig
   ) {}
 
   ngOnInit(): void {
-    this.fetchSelectedUserName();
-    this.fetchEmployees();
-    this.fetchAssessmentSummary();
     this.primengConfig.ripple = true;
+
     if (
       this.currentRoles.includes('HR') ||
       this.currentRoles.includes('SVP') ||
@@ -94,6 +90,20 @@ export class AssessmentSummaryComponent implements OnInit {
     } else {
       this.selectedUserId = this.currentUserId;
     }
+
+    Promise.all([
+      this.fetchSelectedUserDetails(),
+      this.fetchAssessmentSummary(),
+    ])
+      .then(() => {
+        this.createAssessmentSummary();
+      })
+      .finally(() => {
+        this.isLoading = false;
+      })
+      .catch((error) => {
+        console.error('Error while loading assessment summary:', error);
+      });
   }
 
   fetchEmployees(): void {
@@ -139,7 +149,7 @@ export class AssessmentSummaryComponent implements OnInit {
     }
   }
 
-  fetchSelectedUserName(): void {
+  fetchSelectedUserDetails(): void {
     if (!this.selectedUserId || this.selectedUserId === '') {
       console.warn('No user selected.');
       this.selectedUserId = this.currentUserId;
@@ -152,6 +162,12 @@ export class AssessmentSummaryComponent implements OnInit {
         if (user && user.full_name) {
           this.selectedName = user.full_name;
           console.log('Fetched User Full Name:', this.selectedName);
+          this.selectedDivision = user.division_name;
+          console.log('Fetched User Division:', this.selectedDivision);
+          this.selectedPosition = user.position;
+          console.log('Fetched User Position:', this.selectedPosition);
+          this.selectedStatus = user.employee_status;
+          console.log('Fetched User Status:', this.selectedStatus);
         } else {
           console.warn('User full name not found in response.');
           this.selectedName = '';
@@ -201,31 +217,27 @@ export class AssessmentSummaryComponent implements OnInit {
         `Fetching Achievement Summary for ${this.selectedYear} for User ID: ${this.selectedUserId}`
       );
 
-      this.loading = true;
-
       const summaryUrl = `https://lokakarya-be.up.railway.app/assessmentsummary/achievementsummary/${this.selectedUserId}/${this.selectedYear}`;
 
       console.log('Sending Request to URL:', summaryUrl);
 
-      this.http
-        .get<any>(summaryUrl)
-        .pipe(finalize(() => (this.loading = false)))
-        .subscribe({
-          next: (response) => {
-            this.achievements = response.content || [];
-            console.log('Fetched Achievement Summary:', this.achievements);
-            resolve();
-          },
-          error: (error) => {
-            console.error('Error Fetching Achievement Summary:', error);
-            this.messageService.add({
-              severity: 'error',
-              summary: 'Error',
-              detail: 'Failed to fetch achievement summary.',
-            });
-            reject(error);
-          },
-        });
+      this.http.get<any>(summaryUrl).subscribe({
+        next: (response) => {
+          this.achievements = response.content || [];
+          console.log('Fetched Achievement Summary:', this.achievements);
+          resolve();
+        },
+
+        error: (error) => {
+          console.error('Error Fetching Achievement Summary:', error);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Failed to fetch achievement summary.',
+          });
+          reject(error);
+        },
+      });
     });
   }
 
@@ -242,31 +254,26 @@ export class AssessmentSummaryComponent implements OnInit {
         `Fetching Attitude Skill Summary for ${this.selectedYear} for User ID: ${this.selectedUserId}`
       );
 
-      this.loading = true;
-
       const summaryUrl = `https://lokakarya-be.up.railway.app/assessmentsummary/attitudeskillsummary/${this.selectedUserId}/${this.selectedYear}`;
 
       console.log('Sending Request to URL:', summaryUrl);
 
-      this.http
-        .get<any>(summaryUrl)
-        .pipe(finalize(() => (this.loading = false)))
-        .subscribe({
-          next: (response) => {
-            this.attitudeSkills = response.content || [];
-            console.log('Fetched Attitude Skill Summary:', this.attitudeSkills);
-            resolve();
-          },
-          error: (error) => {
-            console.error('Error Fetching Attitude Skill Summary:', error);
-            this.messageService.add({
-              severity: 'error',
-              summary: 'Error',
-              detail: 'Failed to fetch attitude skill summary.',
-            });
-            reject(error);
-          },
-        });
+      this.http.get<any>(summaryUrl).subscribe({
+        next: (response) => {
+          this.attitudeSkills = response.content || [];
+          console.log('Fetched Attitude Skill Summary:', this.attitudeSkills);
+          resolve();
+        },
+        error: (error) => {
+          console.error('Error Fetching Attitude Skill Summary:', error);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Failed to fetch attitude skill summary.',
+          });
+          reject(error);
+        },
+      });
     });
   }
 
@@ -283,8 +290,6 @@ export class AssessmentSummaryComponent implements OnInit {
         `Fetching Attitude Skill Summary for ${this.selectedYear} for User ID: ${this.selectedUserId}`
       );
 
-      this.loading = true;
-
       const summaryUrl =
         'https://lokakarya-be.up.railway.app/empsuggestion/' +
         this.selectedUserId +
@@ -293,25 +298,22 @@ export class AssessmentSummaryComponent implements OnInit {
 
       console.log('Sending Suggestion Request to URL:', summaryUrl);
 
-      this.http
-        .get<any>(summaryUrl)
-        .pipe(finalize(() => (this.loading = false)))
-        .subscribe({
-          next: (response) => {
-            this.suggestion = response.content.suggestion || '';
-            console.log('Fetched Suggestions:', this.suggestion);
-            resolve();
-          },
-          error: (error) => {
-            console.error('Error Fetching Suggestions:', error);
-            this.messageService.add({
-              severity: 'error',
-              summary: 'Error',
-              detail: 'Failed to fetch suggestions.',
-            });
-            reject(error);
-          },
-        });
+      this.http.get<any>(summaryUrl).subscribe({
+        next: (response) => {
+          this.suggestion = response?.content?.suggestion || '';
+          console.log('Fetched Suggestions:', this.suggestion);
+          resolve();
+        },
+        error: (error) => {
+          console.error('Error Fetching Suggestions:', error);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Failed to fetch suggestions.',
+          });
+          reject(error);
+        },
+      });
     });
   }
 
@@ -412,35 +414,37 @@ export class AssessmentSummaryComponent implements OnInit {
   }
 
   createAssessmentSummary(): void {
-    const checkUrl = `https://lokakarya-be.up.railway.app/assessmentsummary/${this.selectedUserId}/${this.selectedYear}`;
+    const checkUrl = `https://lokakarya-be.up.railway.app/assessmentsummary/get/${this.selectedUserId}/${this.selectedYear}`;
     const createUrl =
       'https://lokakarya-be.up.railway.app/assessmentsummary/create';
 
-    interface AssessmentSummary {
-      id: string;
-      user_id: string;
-      year: number;
-      score: number;
-      status: number;
-      created_by: string;
-    }
+    console.log(
+      'Checking for existing Assessment Summary using URL:',
+      checkUrl
+    );
 
     this.http
-      .get<AssessmentSummary>(checkUrl)
+      .get<any>(checkUrl)
       .pipe(
         switchMap((existingSummary) => {
-          if (existingSummary && existingSummary.id) {
-            const updateUrl = `https://lokakarya-be.up.railway.app/assessmentsummary/update/${existingSummary.id}`;
+          const fetchedSummary = existingSummary.content;
+          console.log('Fetched Summary:', fetchedSummary);
+          if (existingSummary && fetchedSummary?.id) {
+            const updateUrl = `https://lokakarya-be.up.railway.app/assessmentsummary/update`;
 
-            const updateBody: Partial<AssessmentSummary> = {
+            const updateBody: any = {
+              id: fetchedSummary.id,
+              user_id: this.selectedUserId,
+              year: this.selectedYear,
               score: this.totalFinalScore,
               status: this.selectedStatus,
+              updated_by: this.currentUserId,
             };
 
             console.log('Existing Summary Found. Updating:', existingSummary);
             console.log('Update Body:', updateBody);
 
-            return this.http.put<AssessmentSummary>(updateUrl, updateBody).pipe(
+            return this.http.put<any>(updateUrl, updateBody).pipe(
               catchError((err) => {
                 console.error('Error updating Assessment Summary:', err);
                 let errorMessage = 'Failed to update assessment summary.';
@@ -456,8 +460,7 @@ export class AssessmentSummaryComponent implements OnInit {
               })
             );
           } else {
-            const requestBody: AssessmentSummary = {
-              id: '',
+            const requestBody: any = {
               user_id: this.selectedUserId,
               year: this.selectedYear,
               score: this.totalFinalScore,
@@ -467,32 +470,38 @@ export class AssessmentSummaryComponent implements OnInit {
 
             console.log('No Existing Summary Found. Creating:', requestBody);
 
-            return this.http
-              .post<AssessmentSummary>(createUrl, requestBody)
-              .pipe(
-                catchError((err) => {
-                  console.error('Error creating Assessment Summary:', err);
-                  let errorMessage = 'Failed to create assessment summary.';
-                  if (err.error && err.error.message) {
-                    errorMessage = err.error.message;
-                  }
-                  this.messageService.add({
-                    severity: 'error',
-                    summary: 'Error',
-                    detail: errorMessage,
-                  });
-                  return of(null);
-                })
-              );
+            return this.http.post<any>(createUrl, requestBody).pipe(
+              catchError((err) => {
+                console.error('Error creating Assessment Summary:', err);
+                let errorMessage = 'Failed to create assessment summary.';
+                if (err.error && err.error.message) {
+                  errorMessage = err.error.message;
+                }
+                this.messageService.add({
+                  severity: 'error',
+                  summary: 'Error',
+                  detail: errorMessage,
+                });
+                return of(null);
+              })
+            );
           }
         })
       )
-      .subscribe((response) => {
-        if (response) {
-          if (response.id) {
-            response.created_by === this.currentUserId ? 'created' : 'updated';
+      .subscribe({
+        next: (response) => {
+          if (response) {
+            console.log('Assessment Summary successfully processed:', response);
+          } else {
+            console.log('No action was taken (possibly due to an error).');
           }
-        }
+        },
+        error: (error) => {
+          console.error('Unhandled error:', error);
+        },
+        complete: () => {
+          console.log('Create Assessment Summary process complete.');
+        },
       });
   }
 }
